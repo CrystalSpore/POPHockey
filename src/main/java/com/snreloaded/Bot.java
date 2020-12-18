ab03ef5071b6409c846cfe542ae2c99042304bde
CrystalSpore <sporeluc@gmail.com>
2020-12-18 13:31:56 -0500

    added code documentation & refactored for better readability
    probably fixed issue with checkNHL, since hardcoded url was in getRequest method

--- src/main/java/com/snreloaded/Bot.java
+++ src/main/java/com/snreloaded/Bot.java
@@ -24,8 +24,7 @@ public class Bot extends ListenerAdapter {
     HashMap<Long, cacheData> cacheMap;
     File nhl_cache;
 
-    public static void main(String[] args) throws LoginException
-    {
+    public static void main(String[] args) throws LoginException {
         if (args.length < 1) {
             System.out.println("You have to provide a token as first argument!");
             System.exit(1);
@@ -36,10 +35,14 @@ public class Bot extends ListenerAdapter {
         JDA instance = JDABuilder.createLight(args[0], GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
                 .setActivity(Activity.playing("Type !popPing"))
                 .build();
+        //We don't immediately add the bot as an event listener, so that we can store the instance into this object
         Bot thisBot = new Bot(instance);
         instance.addEventListener(thisBot);
     }
 
+    /**
+     * data structure for our cached data
+     */
     protected class cacheData {
         long channelID;
         int teamID;
@@ -71,10 +74,11 @@ public class Bot extends ListenerAdapter {
     public Bot(JDA instance) {
         this.instance = instance;
 
+        //path to our cache file (starts with a . so hidden file)
         nhl_cache = new File(System.getProperty("user.home")+"/"+".nhl_cache");
 
-        if ( nhl_cache.exists() )
-        {
+        //if the cache exists, we want to read in from it
+        if ( nhl_cache.exists() ) {
             //Guild/Server, <channel, teamID, numGames>
             cacheMap = new HashMap<>();
             Scanner fin = null;
@@ -84,8 +88,7 @@ public class Bot extends ListenerAdapter {
                 e.printStackTrace();
             }
             if (fin != null) {
-                while ( fin.hasNext() )
-                {
+                while ( fin.hasNext() ) {
                     String line = fin.nextLine();
                     String[] splitLine = line.split(":");
                     cacheMap.put(Long.parseLong(splitLine[0]),
@@ -94,10 +97,9 @@ public class Bot extends ListenerAdapter {
                                     Integer.parseInt(splitLine[3])));
                 }
             }
-        }
-        else
-        {
+        } else { // otherwise create the file
             try {
+                //ignoring output due to checking existence above
                 nhl_cache.createNewFile();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -108,23 +110,78 @@ public class Bot extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         Message msg = event.getMessage();
+        //check entire text or start of text for command, then call method
         if (msg.getContentRaw().equals("!popPing")) {
-            MessageChannel channel = event.getChannel();
-            long time = System.currentTimeMillis();
-            channel.sendMessage("Pong!") /* => RestAction<Message> */
-                    .queue(response /* => Message */ -> {
-                        response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
-                    });
+            ping(event);
         } else if (msg.getContentRaw().equals("!listTeams")) {
-            MessageChannel channel = event.getChannel();
+            listTeams(event);
+        } else if (msg.getContentRaw().startsWith("!setupTeam")
+                || msg.getContentRaw().startsWith("!setup ")) {
+            setupTeam(event);
+        }
+    }
+
+    //ping command, for checking bot latency
+    public void ping(MessageReceivedEvent event) {
+        MessageChannel channel = event.getChannel();
+        long time = System.currentTimeMillis();
+        channel.sendMessage("Pong!") /* => RestAction<Message> */
+                .queue(response /* => Message */ -> {
+                    response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
+                });
+    }
+
+    //list all teams that NHL reports back from API
+    public void listTeams(MessageReceivedEvent event) {
+        MessageChannel channel = event.getChannel();
+        try {
+            //multi-line messages need a MessageBuilder
+            MessageBuilder builder = new MessageBuilder();
+
+            //NHLPolling is the API class
+            builder.appendCodeBlock(NHLPolling.listTeams(),"");
+            Queue<Message> messages = builder.buildAll(MessageBuilder.SplitPolicy.NEWLINE);
+
+            //multi-line message needs a loop to send all lines
+            for (Message m : messages) {
+                channel.sendMessage(m).queue();
+            }
+        } catch (IOException e) {
+            channel.sendMessage("Error w/ NHL api").queue();
+            e.printStackTrace();
+        } catch (ParseException e) {
+            channel.sendMessage("Error parsing team list").queue();
+            e.printStackTrace();
+        }
+    }
+
+    //setup a channel with auto reporting team stats
+    public void setupTeam(MessageReceivedEvent event) {
+        MessageChannel channel = event.getChannel();
+        String[] strings = event.getMessage().getContentRaw().split(" ");
+
+        //help block
+        if ( strings.length == 1 || strings[1].toLowerCase().equals("h") || strings[1].toLowerCase().equals("help") ) {
+            MessageBuilder builder = new MessageBuilder();
+            builder.appendCodeBlock("Format as > !setupTeam <teamID>\n\tSetup NHL team listener in current channel.\n\tUse !listTeams to find the teamID.","");
+            Queue<Message> messages = builder.buildAll(MessageBuilder.SplitPolicy.NEWLINE);
+            for (Message m : messages) {
+                channel.sendMessage(m).queue();
+            }
+        } else { //setup logic block
+            //debug statement, will remove later
+            channel.sendMessage("start").queue();
+
+            //data for cache (not yet implemented)
+            long channelID = channel.getIdLong();
+            long guildID = event.getGuild().getIdLong();
+            int nhlTeamID = Integer.parseInt(strings[1]);
+            int numGames;
             try {
-                MessageBuilder builder = new MessageBuilder();
-                //System.out.println(NHLPolling.listTeams());
-                builder.appendCodeBlock(NHLPolling.listTeams(),"");
-                Queue<Message> messages = builder.buildAll(MessageBuilder.SplitPolicy.NEWLINE);
-                for (Message m : messages) {
-                    channel.sendMessage(m).queue();
-                }
+                //testing message for checkNHL method. currently broken on JSON parse
+                numGames = NHLPolling.checkNHL(nhlTeamID).getGamesPlayed();
+                channel.sendMessage("guild: " + guildID + " | channel: " + channelID + " | nhlTeamID: " + nhlTeamID + " | Number of Games Played: " + numGames).queue();
+                channel.sendMessage("test").queue();
             } catch (IOException e) {
                 channel.sendMessage("Error w/ NHL api").queue();
                 e.printStackTrace();
@@ -132,37 +189,7 @@ public class Bot extends ListenerAdapter {
                 channel.sendMessage("Error parsing team list").queue();
                 e.printStackTrace();
             }
-        } else if (msg.getContentRaw().startsWith("!setupTeam")) {
-            MessageChannel channel = event.getChannel();
-            String[] strings = msg.getContentRaw().split(" ");
-
-            if ( strings.length == 1 || strings[1].toLowerCase().equals("h") || strings[1].toLowerCase().equals("help") ) {
-                MessageBuilder builder = new MessageBuilder();
-                builder.appendCodeBlock("Format as > !setupTeam <teamID>\n\tSetup NHL team listener in current channel.\n\tUse !listTeams to find the teamID.","");
-                Queue<Message> messages = builder.buildAll(MessageBuilder.SplitPolicy.NEWLINE);
-                for (Message m : messages) {
-                    channel.sendMessage(m).queue();
-                }
-            } else {
-                channel.sendMessage("start").queue();
-                Long channelID = channel.getIdLong();
-                Long guildID = event.getGuild().getIdLong();
-                int nhlTeamID = Integer.parseInt(strings[1]);
-                int numGames;
-                try {
-                    numGames = NHLPolling.checkNHL(nhlTeamID).getGamesPlayed();
-                    channel.sendMessage("guild: " + guildID + " | channel: " + channelID + " | nhlTeamID: " + nhlTeamID + " | Number of Games Played: " + numGames).queue();
-                    channel.sendMessage("test").queue();
-                } catch (IOException e) {
-                    channel.sendMessage("Error w/ NHL api").queue();
-                    e.printStackTrace();
-                } catch (ParseException e) {
-                    channel.sendMessage("Error parsing team list").queue();
-                    e.printStackTrace();
-                }
-                channel.sendMessage("end").queue();
-            }
+            channel.sendMessage("end").queue();
         }
-
     }
 }
--- src/main/java/com/snreloaded/NHLPolling.java
+++ src/main/java/com/snreloaded/NHLPolling.java
@@ -8,39 +8,48 @@ import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
 
 public class NHLPolling {
+
+    // GET request without needing external libraries. GET on webAddr
     public static String getRequest(String webAddr) throws IOException {
-        URL url = new URL("http://statsapi.web.nhl.com/api/v1/teams/");
+
+        //setup, connect, & receive response code
+        URL url = new URL(webAddr);
         HttpURLConnection conn = (HttpURLConnection)url.openConnection();
         conn.setRequestMethod("GET");
         conn.connect();
         int responseCode = conn.getResponseCode();
 
         String response = "";
+
+        //if not "OK" status, print what status code was
         if ( responseCode != 200 ) {
             throw new RuntimeException("HttpResponseCode: " + responseCode);
-        } else {
+        } else { //else read & return the data stream
             Scanner sc = new Scanner(url.openStream());
             while ( sc.hasNext() ) {
                 response += sc.nextLine() + "\n";
             }
-            /*System.out.println("\nJSON data in string format");
-            System.out.println(response);*/
             sc.close();
             return response;
         }
     }
 
+    //check stats for specific NHL team (based on NHL team ID number)
     public static NHLStats checkNHL(int teamNumber) throws IOException, ParseException {
+
+        //GET request
         String response = getRequest("http://statsapi.web.nhl.com/api/v1/teams/"+teamNumber+"?expand=team.stats");
+
+        System.out.println(response);
 
         System.out.println("\nBack from GET\n");
 
+        //start JSON parse
         JSONObject initialObject = (JSONObject) (new JSONParser().parse(response));
 
         System.out.println("\nAfter Parser\n");
@@ -67,6 +76,7 @@ public class NHLPolling {
 
         System.out.println("\nAfter JSON manipulation\n");
 
+        //serialize parsed JSON to NHLStats object for ease of use in Java
         NHLStats ret = new Gson().fromJson(stats.toJSONString(), NHLStats.class);
 
         System.out.println("\nBefore Return\n");
@@ -74,11 +84,18 @@ public class NHLPolling {
         return ret;
     }
 
+    //GET & return list of all teams from NHL API
     public static String listTeams() throws IOException, ParseException {
-        String response = getRequest("here");
+
+        //GET request
+        String response = getRequest("http://statsapi.web.nhl.com/api/v1/teams/");
+
+        //Start JSON parse
         JSONObject initialObject = (JSONObject) (new JSONParser().parse(response));
         JSONArray teamsList = (JSONArray) initialObject.get("teams");
 
+        //build list of teams for printing
+        //  (formatting might need to be fixed for :'s to line up)
         JSONObject curTeam = (JSONObject)teamsList.get(0);
         String ret = curTeam.get("id") + "\t:\t" + curTeam.get("name");
         for ( int i = 1; i < teamsList.size(); i++ ) {
